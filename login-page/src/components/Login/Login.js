0c0e37d75fb2858806b51bf6df9b4a85f9e9f19c
Tongyang Ni <ntyaiqingr@gmail.com>
2022-09-29 09:44:23 +0800

    a way to make app.js simple to focus on main component by modifying authcontext component

--- login-page/src/components/Login/Login.js
+++ login-page/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../contexts/auth-context';
 
 const emailReducer = (state , action) => {
   if(action.type === "USER_INPUT") {
@@ -29,6 +30,7 @@ const passwordReducer = (state, action) => {
 }
 
 const Login = (props) => {
+  const authCtx = useContext(AuthContext)
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState('');
@@ -97,7 +99,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
