2e4b0b433574f3ffee98a95aba089bb9c9ccb741
Tongyang Ni <ntyaiqingr@gmail.com>
2022-10-03 16:22:04 +0800

    useMemo to determine when to re-excute the stored function and return a value

--- Behind-the-scenes/src/App.js
+++ Behind-the-scenes/src/App.js
@@ -1,11 +1,13 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import Button from "./components/UI/Button/Button";
 import "./App.css";
 import DemoOutput from "./components/Demo/DemoOutput";
+import DemoList from "./components/Demo/DemoList";
 
 function App() {
 	const [showPara, setShowPara] = useState(false);
     const [showToggle, setShowToggle] = useState(false);
+	const [listTitle, setListTitle] = useState("My List");
 
 	console.log("APP RUNNING");
 
@@ -18,14 +20,22 @@ function App() {
     const showToggleHandler = () => {
         setShowToggle(true);
     }
+
+	const listTitleHandler = useCallback(() => {
+		setListTitle("A new List");
+	}, []);
+
+	const listNum = useMemo(() => [3,4,1,2,7,0], []);
 	return (
 		<div className="app">
 			<h1>Hi there!</h1>
 			{/* {showPara && <p>A new Para</p>} */}
 			{/* hard coded the show value to false. Children are still re-excuted */}
 			<DemoOutput show={false} />
+			<DemoList title={listTitle} items={listNum}></DemoList>
 			<Button onClick={showParaHandler}>Add new Paragraph</Button>
             <Button onClick={showToggleHandler}>Add Toggle</Button>
+			<Button onClick={listTitleHandler}>Sorted List</Button>
 		</div>
 	);
 }
